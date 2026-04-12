23f0931a2b58fc0b1689a63daaa7927a4ddcbb5f
Nathania Layeni <omoebun@yahoo.com>
2026-04-12 22:40:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -378,3 +378,15 @@ function sortTasks() {
 function formatTime(dateStr) {
   return new Date(dateStr).toLocaleString();
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js")
+      .then((reg) => {
+        console.log("SW registered:", reg.scope);
+      })
+      .catch((err) => {
+        console.log("SW failed:", err);
+      });
+  });
+}
